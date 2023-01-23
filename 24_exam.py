66296c9a23e8bd69b8167da34e9a4f1820ac559e
ADDiaz99 <113253264+ADDiaz99@users.noreply.github.com>
2023-01-22 22:16:31 -0500

    Exam

---
+++ 24_exam.py
@@ -0,0 +1,29 @@
+
+
+city = 'NY'
+if city == 'NY':
+    print("Welcome to New York City")
+
+print('Felicidades por aprender Python!')
+
+elements = ['Water', 'Fire', 'Wind', 'Earth']
+for element in elements:
+    print(element)
+
+print(True and False)
+
+pp = [1, 2, 3, 4, 5]
+pp.append(7)
+print(pp)
+
+
+population = 0
+while population < 8.85:
+    population = population + 0.05
+    print("La poblacion menor a la esperada")
+print("La poblacion es de 8.85 millones de habitantes")
+
+
+
+num = int(4.56)
+print(num)
